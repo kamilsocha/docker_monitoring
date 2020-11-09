0f01ad261d1f73cd4f8fa10a0058ed03f1a015dd
kamilsocha <kamil.socha4@gmail.com>
2020-11-09 14:19:12 +0100

    compose

--- edge-service/src/main/java/pl/polsl/student/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/pl/polsl/student/edgeservice/EdgeServiceApplication.java
@@ -1,5 +1,10 @@
 package pl.polsl.student.edgeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,10 +13,20 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
-public class EdgeServiceApplication {
+public class EdgeServiceApplication implements ApplicationRunner {
+
+	@Value("${EUREKA_URI}")
+	private String EUREKA_URI;
+
+	private Logger logger = LoggerFactory.getLogger(EdgeServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdgeServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		logger.warn("EUREKA_URI LOGGING");
+		logger.warn("URI: " + EUREKA_URI);
+	}
 }
